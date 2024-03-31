dfd29f83042a4fecbf5440364f91c1f6358b6b62
zakiafada32 <afadazaki32@gmail.com>
2024-03-31 07:40:10 +0700

    🎉

---
+++ script.js
@@ -0,0 +1,38 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
+
+const marriedTime = new Date(`April 20 2024 09:00:00`);
+console.log(marriedTime);
+
+// Update countdown time
+function updateCountdown() {
+    const currentTime = new Date();
+    console.log(currentTime);
+    const diff = marriedTime - currentTime;
+    console.log(diff);
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+
+    // Add values to DOM
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+}
+
+// Show spinner before countdown
+setTimeout(() => {
+    loading.remove();
+    countdown.style.display = 'flex';
+}, 1000);
+
+// Run every second
+setInterval(updateCountdown, 1000);
